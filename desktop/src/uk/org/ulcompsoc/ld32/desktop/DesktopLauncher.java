1873d7c4d546095a5b96c841331726dbbbbaa2a1
Ashley Davis <ashley_davis10419@hotmail.com>
2015-04-09 16:12:48 +0100

    Initial commit of barebones project

---
+++ desktop/src/uk/org/ulcompsoc/ld32/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.org.ulcompsoc.ld32.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.org.ulcompsoc.ld32.LD32;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD32(), config);
+	}
+}
