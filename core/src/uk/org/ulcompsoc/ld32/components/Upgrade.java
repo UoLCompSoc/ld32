f4bc489eeceafa3840b131e17bada89cb046c426
Solvartier <dg172@student.le.ac.uk>
2015-04-18 16:54:05 +0100

    finished first draft

--- core/src/uk/org/ulcompsoc/ld32/components/Upgrade.java
+++ core/src/uk/org/ulcompsoc/ld32/components/Upgrade.java
@@ -4,25 +4,27 @@ import com.badlogic.ashley.core.Component;
 
 public class Upgrade extends Component {
 	
-	public float dmgMultiplier, timeDelayFiringMultiplier, dropsMultiplier, costMultiplier;
-	
+	public float dmgMultiplier, timeDelayFiringMultiplier, dropsMultiplier, costMultiplier, numberOfFiresToIncreaseBy;
 	public static enum UpgradeType {
-		DAMAGE_PLUS(1.1f, 1.0f,1.0f,1.0f, 1), 
-		SNIPER(1.2f, 0.8f, 1.0f, 1.0f, 1),
-		MORTAR(1.3f, 0.7f, 1.0f, 1.0f, 1), // pure blue
+		DAMAGE_PLUS(1.1f, 1.0f,1.0f,1.0f, 0), 
+		SNIPER(1.2f, 0.8f, 1.0f, 1.0f, 0),
+		MORTAR(1.3f, 0.7f, 1.0f, 1.0f, 0), // pure blue
 		
-		BALL_NUMBER_PLUS_1, BALL_NUMBER_PLUS_2, BALL_NUMBER_PLUS_3,
+		BALL_NUMBER_PLUS_1(1.0f, 1.0f, 1.0f, 1.0f, 1), 
+		BALL_NUMBER_PLUS_2(1.0f, 1.0f, 1.0f, 1.0f, 1), 
+		BALL_NUMBER_PLUS_3(1.0f, 1.0f, 1.0f, 1.0f, 1),
 		// pure red - since there are three stages of the number of Balls I
 		// thought its worth keeping it that way
-		MONSTER_DROPS_1, MONSTER_DROPS_2, UPGRADE_COSTS; // pure green
-		
+		MONSTER_DROPS_1(1.0f, 1.0f, 1.1f, 1.0f, 0),
+		MONSTER_DROPS_2(1.0f, 1.0f, 1.1f, 1.0f, 0),
+		UPGRADE_COSTS(1.0f, 1.0f, 1.0f, 0.9f, 0); // pure green
 		
 		
 		public float dmg;
 		public float time;
 		public float drops;
 		public float costs;
-		public int numberOfSimoultaniousMissiles;
+		public int numberOfSimoultaniousMissiles; // this should be addedto the number of missles fired simoultaiounsly 
 		
 		UpgradeType(float _dmg, float _time, float _drops, float _costs, int number){
 			this.dmg = _dmg;
@@ -31,24 +33,22 @@ public class Upgrade extends Component {
 			this.costs = _costs;
 		}
 	}
-
+	
+	private UpgradeType upgradetype;
 	public Upgrade(UpgradeType upgradeType) {
 		setUpgrade(upgradeType);
 	}
 	//fiddle around with values for the sake of balance
 	private void setUpgrade(UpgradeType upgradeType) {
-		switch (upgradeType) {
-			case DAMAGE_PLUS:
-				this.dmgMultiplier = 1.1f; this.timeDelayFiringMultiplier = 1.0f; this.dropsMultiplier = 1.0f; this.costMultiplier = 1.0f;
-			case SNIPER:
-				this.dmgMultiplier = 1.2f; this.timeDelayFiringMultiplier = 0.8f; this.dropsMultiplier = 1.0f; this.costMultiplier = 1.0f;
-			case MORTAR:
-				this.dmgMultiplier = 1.3f; this.timeDelayFiringMultiplier = 0.7f; this.dropsMultiplier = 1.0f; this.costMultiplier = 1.0f;
-			
-			case BALL_NUMBER_PLUS_1:
-				this.dmgMultiplier = 1.0f; this.timeDelayFiringMultiplier = 1.0f; this.dropsMultiplier = 1.0f; this.costMultiplier = 1.0f;
-			case BALL_NUMBER_PLUS_2:
-				this.dmgMultiplier = 1.0f; this.timeDelayFiringMultiplier = 1.0f; this.dropsMultiplier = 1.0f
-		}
+		this.dmgMultiplier = upgradeType.dmg;
+		this.costMultiplier = upgradeType.costs;
+		this.dropsMultiplier = upgradeType.drops;
+		this.timeDelayFiringMultiplier = upgradeType.time;
+		this.numberOfFiresToIncreaseBy = upgradeType.numberOfSimoultaniousMissiles;
+		this.upgradetype = upgradeType;
+	}
+	
+	public UpgradeType getUpgradeType(){
+		return this.upgradetype;
 	}
 }
