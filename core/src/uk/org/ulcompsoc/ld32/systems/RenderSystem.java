dbf9b863ca8b2e419f9f0cc82f754d164d31478d
Samy Narrainen <samynarrainen@gmail.com>
2015-04-18 19:51:49 +0100

    Health bars
    added a mapper and started work on the render system

--- core/src/uk/org/ulcompsoc/ld32/systems/RenderSystem.java
+++ core/src/uk/org/ulcompsoc/ld32/systems/RenderSystem.java
@@ -1,5 +1,6 @@
 package uk.org.ulcompsoc.ld32.systems;
 
+import uk.org.ulcompsoc.ld32.components.Killable;
 import uk.org.ulcompsoc.ld32.components.Position;
 import uk.org.ulcompsoc.ld32.components.Renderable;
 import uk.org.ulcompsoc.ld32.util.Mappers;
@@ -10,6 +11,7 @@ import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.graphics.Color;
 
 public class RenderSystem extends IteratingSystem {
 	private final ShapeRenderer renderer = new ShapeRenderer();
@@ -36,11 +38,27 @@ public class RenderSystem extends IteratingSystem {
 	protected void processEntity(Entity entity, float deltaTime) {
 		final Position p = Mappers.positionMapper.get(entity);
 		final Renderable r = Mappers.renderableMapper.get(entity);
+		final Killable k = Mappers.killableMapper.get(entity);
 
 		final float w = 5.0f;
 		final float h = 5.0f;
 
 		renderer.setColor(r.color);
 		renderer.rect(p.getX() - (w / 2.0f), p.getY() - (h / 2.0f), w, h);
+
+		if(k != null) {
+			drawHealthBar(p, r, k);
+		}
+	}
+
+	private final Color POSITIVE_HEALTH_COLOR = Color.GREEN;
+	private final Color NEGATIVE_HEALTH_COLOR = Color.RED;
+
+
+	private void drawHealthBar(Position p, Renderable r, Killable k) {
+		renderer.setColor(POSITIVE_HEALTH_COLOR);
+
+		if(r == )
+
 	}
 }
--- core/src/uk/org/ulcompsoc/ld32/util/Mappers.java
+++ core/src/uk/org/ulcompsoc/ld32/util/Mappers.java
@@ -1,13 +1,6 @@
 package uk.org.ulcompsoc.ld32.util;
 
-import uk.org.ulcompsoc.ld32.components.Doomed;
-import uk.org.ulcompsoc.ld32.components.MapRenderable;
-import uk.org.ulcompsoc.ld32.components.PaddleInputListener;
-import uk.org.ulcompsoc.ld32.components.PathFollower;
-import uk.org.ulcompsoc.ld32.components.Position;
-import uk.org.ulcompsoc.ld32.components.Renderable;
-import uk.org.ulcompsoc.ld32.components.Tower;
-import uk.org.ulcompsoc.ld32.components.Upgradable;
+import uk.org.ulcompsoc.ld32.components.*;
 
 import com.badlogic.ashley.core.ComponentMapper;
 
@@ -25,4 +18,5 @@ public class Mappers {
 	public static final ComponentMapper<Doomed> doomedMapper = ComponentMapper.getFor(Doomed.class);
 	public static final ComponentMapper<PaddleInputListener> paddleInputListener = ComponentMapper
 	        .getFor(PaddleInputListener.class);
+	public static final ComponentMapper<Killable> killableMapper = ComponentMapper.getFor(Killable.class);
 }
