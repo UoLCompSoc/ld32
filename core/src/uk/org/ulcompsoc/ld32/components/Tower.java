544d88712e8ffb54c777dad831c09671fbe4ab99
Solvartier <dg172@student.le.ac.uk>
2015-04-18 15:52:08 +0100

    starting things off

--- core/src/uk/org/ulcompsoc/ld32/components/Tower.java
+++ core/src/uk/org/ulcompsoc/ld32/components/Tower.java
@@ -1,16 +1,35 @@
 package uk.org.ulcompsoc.ld32.components;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.badlogic.ashley.core.Component;
 
 public class Tower extends Component {
 
 	private static final float DFLT_RANGE = 10.0f; // starting range
+	private static final float DFLT_FIRE_DELAY = 0.5f; //default fire delay
+	private static final float DFLT_MONSTER_DROP_RATE = 0.1f; // not entirely sure what to make it equal - change if deemed necessary
 	
 	
 	public float range;
+	public float fireDelay;
+	public float dropRate;
+	
+	public int redBalls;
+	public int blueBalls; // heeeeeeeyooooo :D
+	public int greenBalls;
+	
+	public List<RingSegment> listOfPointsToScan;
 	
 	public Tower(){
-		range = Tower.DFLT_RANGE;
+		this.range = Tower.DFLT_RANGE;
+		this.fireDelay = Tower.DFLT_FIRE_DELAY;
+		this.dropRate = Tower.DFLT_MONSTER_DROP_RATE;
+		
+		this.redBalls = 0; this.blueBalls = 0; this.greenBalls = 0;
+		
+		listOfPointsToScan = new ArrayList<RingSegment>();
 	}
 	
 	
---
+++ core/src/uk/org/ulcompsoc/ld32/components/Upgrade.java
@@ -0,0 +1,23 @@
+package uk.org.ulcompsoc.ld32.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class Upgrade extends Component {
+
+	public static enum upgrade {
+		BASIC_TOWER,
+		DAMAGE_PLUS, SNIPER, MORTAR, //pure blue
+		BALL_NUMBER_PLUS_1, BALL_NUMBER_PLUS_2, BALL_NUMBER_PLUS_3,//pure red - since there are three stages of the number of Balls I thought its worth keeping it that way
+		MONSTER_DROPS_1, MONSTER_DROPS_2, MONSTER_DROPS_3 
+	}
+	public Upgrade(upgrade upgrade){
+		setUpgrade(upgrade);
+	}
+	
+	
+	private void setUpgrade(upgrade upgrade){
+		switch(upgrade){
+		
+		}
+	}
+}
