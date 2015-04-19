b333af824e010d2a2b615532f0fc718961fbf11c
Ashley Davis <ashley_davis10419@hotmail.com>
2015-04-19 15:34:53 +0100

    Fix compile error

--- core/src/uk/org/ulcompsoc/ld32/systems/LootDropSystem.java
+++ core/src/uk/org/ulcompsoc/ld32/systems/LootDropSystem.java
@@ -2,6 +2,8 @@ package uk.org.ulcompsoc.ld32.systems;
 
 public class LootDropSystem {
 
-	 @SuppressWarnings("unchecked")
-	 public LootDropSystem()
+	@SuppressWarnings("unchecked")
+	public LootDropSystem() {
+
+	}
 }
