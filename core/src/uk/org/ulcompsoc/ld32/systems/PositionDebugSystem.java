9cc63c677c955085bfa4de87d97302eef4e82da8
Ashley Davis <ashley_davis10419@hotmail.com>
2015-04-19 16:26:49 +0100

    Add position debug system, work on correct offsets

---
+++ core/src/uk/org/ulcompsoc/ld32/systems/PositionDebugSystem.java
@@ -0,0 +1,36 @@
+package uk.org.ulcompsoc.ld32.systems;
+
+import uk.org.ulcompsoc.ld32.components.Position;
+import uk.org.ulcompsoc.ld32.components.Renderable;
+import uk.org.ulcompsoc.ld32.util.Mappers;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+
+public class PositionDebugSystem extends IteratingSystem {
+	private final ShapeRenderer renderer;
+
+	@SuppressWarnings("unchecked")
+	public PositionDebugSystem(int priority, final ShapeRenderer renderer) {
+		super(Family.all(Position.class, Renderable.class).get(), priority);
+		this.renderer = renderer;
+	}
+
+	@Override
+	public void update(float deltaTime) {
+		renderer.begin(ShapeType.Point);
+
+		super.update(deltaTime);
+
+		renderer.end();
+	}
+
+	@Override
+	protected void processEntity(Entity entity, float deltaTime) {
+		final Position p = Mappers.positionMapper.get(entity);
+		renderer.point(p.getX(), p.getY(), 0.0f);
+	}
+}
